9b2c3a8722279c93144560f2674b6b9c9ca81a34
ANAYkrishnaN <115539670+ANAYkrishnaN@users.noreply.github.com>
2026-03-11 14:29:57 -0400

    Added multi OS testing workflow

---
+++ FileManager.java
@@ -0,0 +1,10 @@
+public class FileManager {
+
+    public static void main(String[] args) {
+        System.out.println("FileManager running");
+    }
+
+    public void createFile(String name) {
+        System.out.println("Creating file: " + name);
+    }
+}
